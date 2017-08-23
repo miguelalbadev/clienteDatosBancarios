0dbc64be0aff7e3702e1f78f2b201c68663d662f
java <miguelalbadev@gmail.com>
2017-08-23 15:37:02 +0200

    Actualizada copia local con cambios en maestro

--- src/master.js
+++ src/master.js
@@ -1,32 +1,9 @@
-import Vue from 'vue'
-
 
 Vue.component('master', {
-  template: `
-    <div id="app">
-      <div id="master">
-        <li v-for="persona in personasList">
-          <a @href.prevent="" v-on:click="selectPersona(todo.id)">
-            {{ persona.Nombre }} {{ persona.Apellidos }}
-          </a>
-        </li>
-      </div>
-
-      <detail></detail>
-    </div>
-    `,
-
-  data: {
-      personasList: [],
-      personaSelected: null
-  },
-
-  mounted() {
-    this.cargaListado();
-  },
-
+  props: ['todo'],
+  template: '<li><a @href.prevent="" v-on:click="selectPersona(todo.Id)">{{ todo.Nombre }} {{ todo.Apellidos }}</a></li>',
   methods: {
-    selectPersona(id) {
+    selectPersona:function(id) {
       debugger;
       app2.seen = true;
       $.ajax({
@@ -41,6 +18,7 @@ Vue.component('master', {
         // la respuesta es pasada como argumento a la función
         success: function(data){
           debugger;
+          var i = 0;
           for(i=0;i<data.length;i++){
             var persona = {};
               persona.Id = data[i].Id;
@@ -48,7 +26,7 @@ Vue.component('master', {
               persona.Apellidos = data[i].Apellidos;
               persona.Edad = data[i].Edad;
               persona.Genero = data[i].Genero;
-              this.personasList.push(persona);
+              app.personasList.push(persona);
           }
         },
         error: function(xhr, status) {
@@ -59,46 +37,53 @@ Vue.component('master', {
           //alert('Petición realizada');
         }
       });
-    },
-
-    cargaListado() {
-      $.ajax({
-
-        url: "http://10.60.23.21:50940/api/Personas/",
-        type: 'GET',
-
-        // el tipo de información que se espera de respuesta
-        dataType: 'json',
-
-        // código a ejecutar si la petición es satisfactoria;
-        // la respuesta es pasada como argumento a la función
-        success: function(data) {
-          debugger;
-          for (i = 0; i < data.length; i++) {
-            var persona = {};
-            persona.id = data[i].Id;
-            persona.nombre = data[i].Nombre;
-            persona.apellidos = data[i].Apellidos;
-            persona.edad = data[i].Edad;
-            app.personasList.push(persona);
-          }
-
-        },
-        error: function(xhr, status) {
-          debugger;
-        },
-        // código a ejecutar sin importar si la petición falló o no
-        complete: function(xhr, status) {
-          //alert('Petición realizada');
-        }
-      });
-    }
+    
+     }   
   }
 })
 
 var app = new Vue({
   el: '#app',
-  created(){
-    window.Vue = 'this';
+  data: {
+    personasList: []
+  },
+  mounted:function(){
+    this.cargaListado();
+  },
+  methods:{
+    cargaListado(){
+      $.ajax({
+
+      url:"http://10.60.23.21:50940/api/Personas/",
+        type : 'GET',
+
+        // el tipo de información que se espera de respuesta
+        dataType : 'json',
+     
+        // código a ejecutar si la petición es satisfactoria;
+        // la respuesta es pasada como argumento a la función
+        success : function(data){
+          debugger;
+          var i = 0;
+          for(i=0;i<data.length;i++){
+            var persona = {};
+              persona.Id = data[i].Id;
+              persona.Nombre = data[i].Nombre;
+              persona.Apellidos = data[i].Apellidos;
+              persona.Edad = data[i].Edad;
+              persona.Genero = data[i].Genero;
+              app.personasList.push(persona);
+          }
+          
+        },
+      error: function(xhr, status){
+        debugger;
+      },
+        // código a ejecutar sin importar si la petición falló o no
+        complete : function(xhr, status) {
+            //alert('Petición realizada');
+        }
+    });
+    }
   }
 })
